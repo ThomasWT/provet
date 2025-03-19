78922d752738f92c21d19c56b3d0ceff689f5c5a
Thomas <twt@outlook.dk>
2025-03-20 00:44:12 +0100

    Removed iphone test

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,12 +25,6 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-    {
-      name: 'Mobile Safari',
-      use: {
-        ...devices['iPhone 13'],
-      },
-    },
   ],
   webServer: {
     command: 'pnpm dev',
